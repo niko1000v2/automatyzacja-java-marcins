30d462ba5fce13b0ec86398779f24a43ef40c6ca
Marcin Slipek <mslipek@gmail.com>
2018-12-11 14:52:24 +0100

    pierwsza wersja projektu automatyzacja testow
    Please enter the commit message for your changes. Lines starting

---
+++ autojava/src/main/java/kalkulator/KalkulatorApp.java
@@ -0,0 +1,17 @@
+package kalkulator;
+
+public class KalkulatorApp {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+}
+
